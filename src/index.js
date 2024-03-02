5b573c730d1d2e75d36d935b9aea8252590d9f5b
Kaustubh <kaustubhwahane55@gmail.com>
2024-03-02 14:54:43 +0530

    MongoConnection

--- src/index.js
+++ src/index.js
@@ -9,8 +9,34 @@ app.get("/",(req,res) =>{
     res.send({message : "Hello"});
 })
 
-app.listen(PORT, ()=>{
-    console.log(`It's running on this server ${PORT}`);
-    console.log(`Base URL: http:://127.0.0.1: ${PORT}`);
-})
+async function Connection (){
+    try {
+       await mongoose.connect(MONGODB_URL);
+       app.listen(PORT, ()=>{
+        console.log(`It's running on this server ${PORT}`);
+        console.log(`Base URL: http:://127.0.0.1: ${PORT}`);
+    });
+    } catch(error) {
+        console.log(error);
+}
+}
 
+// Connection();
+
+// Start server first then Connect to MongoDB
+
+const startServerFirst = () => {
+    app.listen(PORT , async () => {
+        try {
+            console.log(`It's running on this server ${PORT}`);
+            console.log(`Base URL: http://127.0.0.1:${PORT}`);
+            await mongoose.connect(MONGODB_URL);
+            console.log("DB Connected!");
+          } catch (error) {
+            console.log(error?.message);
+          }   
+    })
+
+}
+
+startServerFirst()
